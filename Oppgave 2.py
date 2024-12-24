9fb7758c960792ce8eb503290fafd11081d36479
Sazzix <sindregronvold@gmail.com>
2024-12-24 16:32:53 +0100

    Add files via upload

---
+++ Oppgave 2.py
@@ -0,0 +1,6 @@
+#Oppgave 2
+antall_elever = int(input('Skriv inn antall elever:' ))
+
+pizza = antall_elever*1/4
+
+print("På butkken må det handles inn", round(pizza),"pizza.")
---
+++ Oppgave 3.py
@@ -0,0 +1,7 @@
+#Oppgave 3
+import numpy as np
+
+v_grad = float(input('Skriv inn gradtallet:' ))
+v_rad = v_grad*np.pi/180
+
+print("Resultatet fra grader til rader er:", v_rad)
